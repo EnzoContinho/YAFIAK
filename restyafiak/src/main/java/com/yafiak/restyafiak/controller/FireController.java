34c5cf355db7e4b3f56590fc77217fbb1143d171
EnzoContinho <enzo.contini@cpe.fr>
2020-12-16 18:14:34 +0100

    Handling APIs for Fires

--- restyafiak/src/main/java/com/yafiak/restyafiak/controller/FireController.java
+++ restyafiak/src/main/java/com/yafiak/restyafiak/controller/FireController.java
@@ -3,8 +3,10 @@ package com.yafiak.restyafiak.controller;
 import com.yafiak.restyafiak.model.Fire;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.yafiak.restyafiak.repository.FireRepository;
@@ -15,14 +17,35 @@ public class FireController {
 	@Autowired
 	private FireRepository fireRepository;
 	
-	@GetMapping("/fires")
+	@GetMapping("api/fires")
     public List<Fire> getFires() {
         return fireRepository.findAll();
     }
 	
-	@PostMapping("/fires")
+	@GetMapping("api/fires/{id}")
+	@ResponseBody
+    public Optional<Fire> getFireById(@PathVariable Long id) {
+        return fireRepository.findById(id);
+    }
+	
+	@PostMapping("api/fires")
 	public Fire createFire(@RequestBody Fire fire) {
 		return fireRepository.save(fire);
+	}
+	
+	@PutMapping("api/fires/{id}")
+	public ResponseEntity<Object> updateFire(@RequestBody Fire fire, @PathVariable long id) {
+		Optional<Fire> fireOptional = fireRepository.findById(id);
+		if (!fireOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		fire.setId(id);
+		fireRepository.save(fire);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("api/fires/{id}")
+	public void deleteFire(@PathVariable long id) {
+		fireRepository.deleteById(id);
 	}
 	
 }
