e13088c794f9cf7e8f091a22ca5e9b31c2773115
Enzo Contini <enzo.contini@cpe.fr>
2020-12-25 19:44:27 +0100

    Add API to get firetrucks linked to a firestation (repository)

--- restyafiak/src/main/java/com/yafiak/restyafiak/repository/FireTruckRepository.java
+++ restyafiak/src/main/java/com/yafiak/restyafiak/repository/FireTruckRepository.java
@@ -1,8 +1,15 @@
 package com.yafiak.restyafiak.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.yafiak.restyafiak.model.FireTruck;
 
 public interface FireTruckRepository extends JpaRepository<FireTruck, Long> {
+	
+	@Query(value = "SELECT * FROM t_firetruck_ftr ftr INNER JOIN t_firestation_fst fst ON ftr.ftr_firestation_id = fst.fst_id WHERE ftr.ftr_firestation_id = :firestationId", nativeQuery = true)
+	List<FireTruck> findFireTrucksByFireStationId(@Param("firestationId") Long firestationId);
 }
