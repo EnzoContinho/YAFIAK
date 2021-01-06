56215e74d9cc5cc1665f7817487545718ceada84
briangasparini <62886416+briangasparini@users.noreply.github.com>
2021-01-06 12:09:53 +0100

    (#5) FIX - Http request & id in database

--- InfluxDB/MQTT_Client/main.py
+++ InfluxDB/MQTT_Client/main.py
@@ -5,12 +5,8 @@ import paho.mqtt.client as mqtt
 import mysql.connector
 conn = mysql.connector.connect(host="127.0.0.1",user="Mqtt_Client", password="ClientMQTT",database="influxdb")
 
-# 48.862725,60.854657,255
-# id = random()
-# latitude X float
-# Longitude Y float
-# Intensité float
-def updateInfluxDB(locationX,locationY,intenistyFire):
+
+def updateInfluxDB(locationX,locationY,intensityFire):
     cursor = conn.cursor()
     requeteSQL = "SELECT  idFireData FROM fireData WHERE FD_LocationX=%s AND FD_LocationY=%s"
     cursor.execute(requeteSQL,((str(locationX),str(locationY))))
@@ -19,17 +15,20 @@ def updateInfluxDB(locationX,locationY,intenistyFire):
         for x in myresult:
             val = x[0]
         requeteSQL = "UPDATE fireData SET FD_Intensity = %s WHERE idFireData=%s"
-        cursor.execute(requeteSQL,(str(intenistyFire),val))
+        cursor.execute(requeteSQL,(str(intensityFire),val))
         conn.commit()
         print("UPDATE")
     elif (len(myresult) == 0):
         requeteSQL = "INSERT INTO fireData (FD_Intensity,FD_LocationX,FD_LocationY) VALUES (%s,%s,%s)"
-        cursor.execute(requeteSQL,(str(intenistyFire),str(locationX),str(locationY)))
+        cursor.execute(requeteSQL,(str(intensityFire),str(locationX),str(locationY)))
         conn.commit()
         print("INSERT")
     else:
         print("erreur")
-
+    requeteSQL = "INSERT INTO firedata_all (FDA_Intensity) VALUES ('%s')"
+    cursor.execute(requeteSQL,str(intensityFire))
+    conn.commit()
+    print("INSERT")
     
 
 
@@ -41,13 +40,13 @@ def on_message(client, userdata, message):
     sLocationY = msg.split("|")[2]
     sIntensity = msg.split("|")[3]
     print(sId,sLocationX,sLocationY,sIntensity)
-    #updateInfluxDB(random.randint(0,9),random.randint(0,5),random.randint(0,100))
+    updateInfluxDB(random.randint(0,9),random.randint(0,5),random.randint(0,100))
 
 if __name__ == "__main__" :
     print("Début du programme")
     client =mqtt.Client("InfluxDb_MQTT_Client")
     client.connect("127.0.0.1")
-    client.subscribe("test")
+    client.subscribe("Fire")
     client.on_message=on_message 
     client.loop_start()
     mycursor = conn.cursor()
--- yafiak_python_server/YAFIAK_python_server/main.py
+++ yafiak_python_server/YAFIAK_python_server/main.py
@@ -2,14 +2,43 @@ import serial, time
 import threading
 import multiprocessing as mp
 import paho.mqtt.client as mqtt
-
+import json
+# importing the requests library 
+import requests 
 # send serial message 
 # Don't forget to establish the right serial port ******** ATTENTION
-# SERIALPORT = "/dev/ttyUSB0"
+
 SERIALPORT = "COM9"
 BAUDRATE = 115200
 ser = serial.Serial()
 bOk = mp.Value("i",True)
+
+def valuesCorrect(sIdFire,sLocationX,sLocationY,sFireIntensity):
+    if sIdFire == "" or sLocationX == "" or sLocationY == "" or sFireIntensity == "":
+        return False
+    if int(sIdFire) <= 0 :
+        return False
+    if int(sLocationX) < 0 or int(sLocationX) > 9 :
+        return False
+    if int(sLocationY) < 0 or int(sLocationY) > 5 :
+        return False
+    if int(sFireIntensity) < 0 or int(sFireIntensity) > 100 :
+        return False
+    return True
+
+
+def pushFireHTTP(sLocationX,sLocationY,sFireIntensity):
+    # api-endpoint 
+    URL = "http://localhost:8080/api/sensors"
+    # defining a params dict for the parameters to be sent to the API 
+    # sending get request and saving the response as response object 
+    PARAM = json.dumps({'lX': int(sLocationX), 'cY': int(sLocationY), 'intensity' : int(sFireIntensity)})
+    print(PARAM)
+    headers = {"Content-Type": "application/json"}
+    r = requests.put(url = URL, data = PARAM, headers=headers)
+    # extracting data in json format 
+    print(r)
+
 def initUART():     
     # ser = serial.Serial(SERIALPORT, BAUDRATE)
     ser.port=SERIALPORT
@@ -18,7 +47,6 @@ def initUART():
     ser.parity = serial.PARITY_NONE #set parity check: no parity
     ser.stopbits = serial.STOPBITS_ONE #number of stop bits
     ser.timeout = None          #block read
-
     # ser.timeout = 0             #non-block read
     # ser.timeout = 2              #timeout block read
     ser.xonxoff = False     #disable software flow control
@@ -38,35 +66,39 @@ def sendUARTMessage(msg):
     bOk.value = False
 
 def receiveUARTMessage():
-        while ser.isOpen() : 
-                dataLength = 27
-                time.sleep(1)
-                dataBytes = bytearray()
-                if (ser.inWaiting() > 0): # if incoming bytes are waiting 
-                    bOk.value = True
-                    while dataLength != 0:
-                        dataBytes += (ser.read(dataLength))
-                        dataLength -= len((dataBytes))
-                    sid = str((dataBytes[0:4]))
-                    strLocationX = str((dataBytes[4:15]))
-                    strLocationY = str((dataBytes[16:26]))
-                    strFireIntensity = str((dataBytes[26]))
-                    strLocationX = strLocationX.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
-                    strLocationY = strLocationY.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
-                    sid = sid.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
-                    print("------------------------")
-                    print(str(strFireIntensity))
-                    print((strLocationX))
-                    print((strLocationY))
-                    print(sid)
-                    client.publish("test",sid+"|"+strLocationX+"|"+strLocationY+"|"+strFireIntensity)
+    nMessageRecu,nMessageEchec = 0,0
+    while ser.isOpen() : 
+            dataLength = 27
+            time.sleep(1)
+            dataBytes = bytearray()
+            if (ser.inWaiting() > 0): # if incoming bytes are waiting 
+                print("------------------------")
+                print("Message reçu")
+                bOk.value = True
+                while dataLength != 0:
+                    dataBytes += (ser.read(dataLength))
+                    dataLength -= len((dataBytes))
+                sid = str((dataBytes[0:4]))
+                strLocationX = str((dataBytes[4:15]))
+                strLocationY = str((dataBytes[16:26]))
+                strFireIntensity = str((dataBytes[26]))
+                strLocationX = strLocationX.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
+                strLocationY = strLocationY.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
+                sid = sid.replace('bytearray(b','').replace('\\x00','').replace("'","").replace(')','')
+                print("------------------------")
+                if valuesCorrect(sid,strLocationX,strLocationY,strFireIntensity):
+                    nMessageRecu+= 1
+                    print("Id: " + sid)
+                    print("Location X: " + strLocationX)
+                    print("Location Y: " + strLocationY)
+                    print("Intensité Fire: " + strFireIntensity)
+                   # client.publish("Fire",sid+"|"+strLocationX+"|"+strLocationY+"|"+strFireIntensity)
+                    pushFireHTTP(strLocationX,strLocationY,strFireIntensity)
+                else:
+                    nMessageEchec += 1
+                print("Nombre de message reçu / Nombre de message non reçu : " + str(nMessageRecu) + " / " + str(nMessageEchec))
+                    # Requête HTTP POUR PUSH LE FEU
 
-
-# 48.862725,60.854657,255
-# id = random()
-# latitude X float
-# Longitude Y float
-# Intensité float
 
 if __name__ == "__main__" :
     print("Début du programme")
@@ -75,8 +107,7 @@ if __name__ == "__main__" :
     serverSerial_thread = threading.Thread(target=receiveUARTMessage)
     serverSerial_thread.daemon = True
     serverSerial_thread.start()
-    client =mqtt.Client("SP_EmergencyManager")
+    client = mqtt.Client("SP_EmergencyManager")
     client.connect("127.0.0.1")
     while(True):
         time.sleep(10)
-        #client.publish("test","ON")
